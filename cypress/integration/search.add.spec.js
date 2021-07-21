0532ac09711c915bb7b2cd500c2b7cdfa6197ee2
mihaelapopescu2011@gmail.com <mihaelapopescu2011@gmail.com>
2021-07-21 12:57:23 +0300

    Completed test suite for search and add items

--- cypress/integration/search.add.spec.js
+++ cypress/integration/search.add.spec.js
@@ -8,7 +8,6 @@ describe('Searching an item', () => {
         it('1. Search for an existent item', () => {
             cy.get('input[name="filter_keyword"]').type('Paper Towns by John Green{enter}')
             cy.get('.productname').contains('Paper Towns by John Green')
-            cy.log('Item searched was successfully displayed')
         })
 
         it('2. Search for an inexistent item', () => {
@@ -17,27 +16,62 @@ describe('Searching an item', () => {
             cy.log('Item was not found (as expected)')
         })
   
-        it('3. Search for an existent item using the selection list', () => {
+        it('3. Search for existent items using the selection list', () => {
             cy.get('#filter_keyword').click()
-            cy.get('#category_65').click()
-            cy.get('#category_selected').contains('Books').click()
-            cy.get('.maintext').contains('Books')
-            cy.get('ul.thumbnails > :nth-child(2) > :nth-child(1) > img').click()
-            cy.get(':nth-child(1) > .thumbnail > :nth-child(1) > img').click()
-            cy.get('.productname').contains('Paper Towns by John Green')
-            cy.log('Item searched was successfully displayed')
+            cy.get('#category_65').click().type('{enter}')
+            cy.get('#category_id').should('contain','Books')
+            cy.get('#keyword').type('book')
+            cy.get('#description').check()
+            cy.get('#search_button').click()
+            cy.get(':nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').contains('New')
+            cy.get(':nth-child(2) > .fixed_wrapper > .fixed > .prdocutname').contains('Morning')
         })
 
         it('4. Search using the magnifier', () => {
             cy.get('#filter_keyword').get('.button-in-search > .fa').click()
             cy.get('#keyword').type('book')
-            cy.get('#category_id').click().type('Books{enter}')
+            cy.get('#category_id').select('Books').should('have.value', '0,65')
             cy.get('#description').check()
             cy.get('#model').check()
             cy.get('#search_button').click()
-            
-            //cy.log('Item was found (as expected)')
+            cy.get(':nth-child(2) > .thumbnail > :nth-child(1) > img').click()
+            cy.get('.productname').contains('The Miracle Morning: The Not-So-Obvious Secret Guaranteed to Transform Your Life')
           })
 
-  
+          it('5. Search invalid citeria in category using the magnifier', () => {
+            cy.get('#filter_keyword').get('.button-in-search > .fa').click()
+            cy.get('#keyword').type('book')
+            cy.get('#category_id').select('Makeup').should('have.value', '0,36')
+            cy.get('#description').check()
+            cy.get('#search_button').click()
+            cy.get('.maintext').contains('Search')
+            cy.get('.contentpanel > :nth-child(4)').contains('There is no product that matches the search criteria.')
+          })     
   })
+
+describe('Add an item to cart', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('1. Add an existent item from Home page to cart', () =>{
+        cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(1) > .thumbnail > .pricetag > .productcart > .fa').click()
+        cy.get('#topnav > .form-control').select('Cart')
+        cy.get('#cart_quantity68').should('have.value', '1')
+        cy.get('tbody > :nth-child(2) > :nth-child(2) > a').click()
+        cy.url().should('include', '/product&product_id=68&key=68')
+        
+    })
+
+    it('2. Add an existent item from category page to cart', () =>{
+        cy.get('[href="https://automationteststore.com/index.php?rt=product/category&path=43"]').click()
+        cy.get('ul.thumbnails > :nth-child(2) > :nth-child(1) > img').click()
+        cy.get(':nth-child(1) > .thumbnail > :nth-child(1) > img').click()
+        cy.get('.cart').click()
+        cy.get('#topnav > .form-control').select('Cart')
+        cy.get('#cart_quantity66').should('have.value', '1')
+        cy.get('.table > tbody > :nth-child(2) > :nth-child(2) > a').click()
+        cy.url().should('include', 'product&product_id=66&key=66')
+    })
+
+})
