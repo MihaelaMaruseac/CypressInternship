d82f77cf614888d71b384b2e418530071c149111
mihaelapopescu2011@gmail.com <mihaelapopescu2011@gmail.com>
2021-07-22 09:59:57 +0300

    Search and add an item, finalize a purchase with saving order id and email, check the order status, brand selection and sorting, check out of stock items

--- cypress/integration/brandSS.spec.js
+++ cypress/integration/brandSS.spec.js
@@ -9,26 +9,30 @@ describe('Brand selection and sorting', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(1) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', 'sort=p.price-ASC&limit=20')
-            // cy.get('.thumbnail > .pricetag > .price > .pricenew').invoke('text').expect(price =>{
-            // const $el = Cypress.$(cy.get(':nth-child(4) > .thumbnail > .pricetag > .price > .oneprice')
-            // if ($el.text() == price){
-            //     cy.log('Positive')
-            // }
-            // else{
-            //     cy.log('Negative')
-            // }
+            
+            const elements = [];
+            const unsortedElements = [];
+            cy.get('.price').each(($el, $index) =>{
+                if($el[0].parentNode.className === 'pricetag jumbotron'){ // to remove duplicates
+                    elements.push($el);
+                    unsortedElements.push($el)
+                }
+            }).then(() => {
+                elements.sort((a,b)=>{
+                    const firstValue = parseFloat(a[0].children[0].innerHTML.split('$')[1]); // children[0] has either oneprice class or pricenew class
+                    const secondValue = parseFloat(b[0].children[0].innerHTML.split('$')[1]);
+                    return firstValue-secondValue;
+                });
+                cy.wrap(unsortedElements).should('deep.eq', elements);
+            })
         })
 
         it('2. Select and sort products for Pantene brand', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(2) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', '17&sort=p.price-ASC&limit=20')
 
@@ -38,8 +42,6 @@ describe('Brand selection and sorting', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(3) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', '11&sort=p.price-ASC&limit=20')
 
@@ -49,74 +51,43 @@ describe('Brand selection and sorting', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(4) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', 'id=15&sort=p.price-ASC&limit=20')
         })
 
-        it('5. Select and sort products for Gucci brand', () => {
-
-            cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
-            cy.get(':nth-child(5) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length','1')
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length','2')
-            cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
-            cy.url().should('include', 'id=20&sort=p.price-ASC&limit=20')
-        })
-
-        it('6. Select and sort products for Giorgio Armani brand', () => {
+        it('5. Select and sort products for Giorgio Armani brand', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(6) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', 'id=19&sort=p.price-ASC&limit=20')
         })
 
-        it('7. Select and sort products for Dove brand', () => {
+        it('6. Select and sort products for Dove brand', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(7) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', 'id=18&sort=p.price-ASC&limit=20')
         })
 
-        it('8. Select and sort products for Calvin Klein brand', () => {
+        it('7. Select and sort products for Calvin Klein brand', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(8) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', 'id=13&sort=p.price-ASC&limit=20')
         })
 
-        it('9. Select and sort products for Bvlgari brand', () => {
+        it('8. Select and sort products for Bvlgari brand', () => {
 
             cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
             cy.get(':nth-child(9) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
             cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
             cy.url().should('include', 'id=14&sort=p.price-ASC&limit=20')
         })
 
-        it.only('10. Select and sort products for Sephora brand', () => {
-
-            cy.get('#popularbrands > .container-fluid').should('contain', 'Brands Scrolling List')
-            cy.get(':nth-child(10) > .image > a > .internal').click()
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',1)
-            cy.get('.div.thumbnails.grid.row.list-inline').should('not.have.length',2)
-            cy.skip()
-            cy.get('#sort').select('Price Low > High').should('have.value','p.price-ASC')
-            cy.url().should('include', 'id=16&sort=p.price-ASC&limit=20')
-        })
-
-        it('11. Check out of stock items', () =>{
+        it('9. Check out of stock items', () =>{
             cy.get('.nostock').contains('Out of Stock').click()
             cy.log('Find out of stock product')
         })
--- cypress/integration/finalizePurchase.spec.js
+++ cypress/integration/finalizePurchase.spec.js
@@ -4,6 +4,7 @@ describe('Finalize a purchase & check the order status', () => {
    // beforeEach(() => {
     //  cy.visit('/')
   //  })
+    let orderId, email;
 
         it('1. Finalize a purchase', () =>{
             
@@ -30,45 +31,57 @@ describe('Finalize a purchase & check the order status', () => {
             cy.url().should('include', 'checkout/success')
             cy.get('.mb40 > :nth-child(4) > a').click()
             cy.get('.contentpanel > :nth-child(1) > .table > tbody > tr > :nth-child(1) > :nth-child(1)')
-            //save OrderID
+            //save OrderID and email
+            
             cy.get("#maincontainer > div > div > div > div > div:nth-child(1) > table > tbody > tr > td:nth-child(1)").invoke("text").then((text)=>{
-                console.log(text.slice())
+                cy.log("35: ", text)
+                orderId = text.substring(text.indexOf("Order ID")+ "Order ID".length, text.indexOf("Status")).trim().split('#')[1];
+                cy.log("38:", orderId)
+                email = text.substring(text.indexOf("E-Mail")+ "E-Mail".length, text.indexOf("Telephone")).trim();
+                cy.log(email)
             })
+        })
         
-            // cy.get('#maincontainer > div > div > div > div > div:nth-child(1) > table > tbody > tr > td:nth-child(1) > br:nth-child(2)')
-            //     .invoke('val')
-            //     .then(orderID => cy.log(orderID));
-
-            // save email
+        it('2. Try to finalize a purchase with invalid email address', () =>{
+            
+          cy.visit('/')
+          cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(1) > .thumbnail > .pricetag > .productcart > .fa').click()
+          cy.get('#topnav > .form-control').select('Cart')
+          cy.get('#cart_quantity68').should('have.value', '1')
+      
+          cy.get('#cart_checkout2').click()
+          cy.get('#accountFrm_accountguest').check()
+          cy.get('#accountFrm > fieldset > .btn').click()
+          cy.get('#guestFrm_firstname').type('U123')
+          cy.get('#guestFrm_lastname').type('Fake!@!')
+          cy.get('#guestFrm_email').type('123423a')
+          cy.get('#guestFrm_telephone').type('098653873878')
+          cy.get('#guestFrm_address_1').type('str. AAA')
+          cy.get('#guestFrm_city').type('Bristol')
+          cy.get('select[name="zone_id"]').select('Bristol')
+          cy.get('input[name="postcode"]').type('postcode')
+          cy.get('select[name="country_id"]').type('United Kingdom')
+          cy.get('.col-md-12 > .btn-orange').click()
+      
+          cy.get('.has-error > .help-block').contains('E-Mail Address does not appear to be valid!')
         })
 
-        // it('2. Check the order status', () =>{
-            
-                
-        // })
+        it('3. Check the order status', () =>{
+          cy.visit('/')
+          cy.get('#topnav > .form-control').select('Check Your Order')
+          cy.get('#CheckOrderFrm_order_id').type(orderId)
+          cy.get('#CheckOrderFrm_email').type(email)
+          cy.get('.col-md-12 > .btn-orange').click()
+          cy.get('.container-fluid > .table > tbody > :nth-child(2) > :nth-child(2)').contains('Pending')
+        })
 
-        it('3. Try to finalize a purchase with invalid email address', () =>{
-            
-            cy.visit('/')
-            cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(1) > .thumbnail > .pricetag > .productcart > .fa').click()
-            cy.get('#topnav > .form-control').select('Cart')
-            cy.get('#cart_quantity68').should('have.value', '1')
-        
-            cy.get('#cart_checkout2').click()
-            cy.get('#accountFrm_accountguest').check()
-            cy.get('#accountFrm > fieldset > .btn').click()
-            cy.get('#guestFrm_firstname').type('U123')
-            cy.get('#guestFrm_lastname').type('Fake!@!')
-            cy.get('#guestFrm_email').type('123423a')
-            cy.get('#guestFrm_telephone').type('098653873878')
-            cy.get('#guestFrm_address_1').type('str. AAA')
-            cy.get('#guestFrm_city').type('Bristol')
-            cy.get('select[name="zone_id"]').select('Bristol')
-            cy.get('input[name="postcode"]').type('postcode')
-            cy.get('select[name="country_id"]').type('United Kingdom')
-            cy.get('.col-md-12 > .btn-orange').click()
-        
-            cy.get('.has-error > .help-block').contains('E-Mail Address does not appear to be valid!')
-          })
+        it('4. Check the order status with invalid data', () =>{
+          cy.visit('/')
+          cy.get('#topnav > .form-control').select('Check Your Order')
+          cy.get('#CheckOrderFrm_order_id').type('orderId')
+          cy.get('#CheckOrderFrm_email').type('email@gmail.com')
+          cy.get('.col-md-12 > .btn-orange').click()
+          cy.get('.has-error > .help-block').contains('Order ID is required field!')
+        })
 
 })
