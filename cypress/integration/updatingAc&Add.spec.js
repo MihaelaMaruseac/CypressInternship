d1abba881fe3645e7e52de8a9c58a788f1fdbfa7
mihaelapopescu2011@gmail.com <mihaelapopescu2011@gmail.com>
2021-07-22 15:13:06 +0300

    Check wishlist for customer with dynaimc data

--- cypress/integration/updatingAc&Add.spec.js
+++ cypress/integration/updatingAc&Add.spec.js
@@ -2,10 +2,9 @@
 
 describe('Updating Account details', () => {
     beforeEach(() => {
-      cy.visit('/')
+        cy.visit('/')
 
-      cy.get('a[href*="/index.php?rt=account/login"]').contains('Login or register').click()
-
+        cy.get('a[href*="/index.php?rt=account/login"]').contains('Login or register').click()
         cy.get('input[name="loginname"]').type('Testing01')
         cy.get('input[name="password"]').type('Test123!')
         cy.get('button[type="submit"][title="Login"]').click()
@@ -19,13 +18,27 @@ describe('Updating Account details', () => {
         cy.get('#AccountFrm_telephone').clear().type('2345765432').should('have.value', '2345765432')
         cy.get('.col-md-12 > .btn-orange').click()
         cy.get('.alert').contains('Success: Your account has been successfully updated.')
-        // reset the email address for avoiding fail check oreder status
+        // checking if the fields was correctly updated
         cy.get('.side_account_list > :nth-child(3) > a').click()
+        cy.get('#AccountFrm_firstname').should('have.value','Soft')
+        cy.get('#AccountFrm_lastname').should('have.value','Test')
+        cy.get('#AccountFrm_email').should('have.value','test123@gmail.com')
+        cy.get('#AccountFrm_telephone').should('have.value', '2345765432')
+        // retype the email address for avoiding fail check oreder status
         cy.get('#AccountFrm_email').clear().type('testing@gmail.com').should('have.value','testing@gmail.com')
         cy.get('.col-md-12 > .btn-orange').click()
     })
 
-    it('2. Add a new default address', () => {
+    it('2. Edit account details with invalid email address', () => {
+        cy.get('.side_account_list > :nth-child(3) > a').click()
+        cy.get('#AccountFrm_email').clear().type('test123').should('have.value','test123')
+        cy.get('.col-md-12 > .btn-orange').click()
+        cy.get('.alert').contains('Oops, there is an error with information provided!')
+        cy.get('.col-md-12 > .btn-default').click()
+        cy.get('.side_account_list > :nth-child(3) > a').click()
+    })
+
+    it('3. Add a new default address', () => {
         cy.get('.side_account_list > :nth-child(5) > a').click()
         cy.get('.col-md-12 > .btn-orange').click()
         cy.get('#AddressFrm_firstname').type('User')
@@ -40,18 +53,10 @@ describe('Updating Account details', () => {
         cy.get('.alert').contains('Your address has been successfully inserted')
     })
 
-    it.only('3. Remove the old address', () => {
+    it('4. Remove the old address', () => {
         cy.get('.side_account_list > :nth-child(5) > a').click()
-        cy.get('button').each(() =>{
-            cy.get(':nth-child(2) > table > tbody > tr > .pull-right > .btn-default').click()
-        })
+        cy.get('tr > .pull-right > .btn-default').click()
         cy.get('.alert').contains('Your address has been successfully deleted')
-
-
     })
-
-
-  
-  
   
   })
