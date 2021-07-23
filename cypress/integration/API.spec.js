a3fc4e51f56a8dde68743a8df3fc9d1afd3a3ff6
mihaelapopescu2011@gmail.com <mihaelapopescu2011@gmail.com>
2021-07-23 15:43:07 +0300

    API request

--- cypress/integration/API.spec.js
+++ cypress/integration/API.spec.js
@@ -1,19 +1,19 @@
 /// <reference types="cypress" />
 
 describe('Check API', () => {
-    beforeEach(() => {
-      
-      cy.visit('https://automationteststore.com/')
-    })
-  
-    it('Check API Requests', () => {
-        cy.request('https://petstore.swagger.io/v2/store/inventory').then((result)=>{
-            console.log(result.body)
-            console.table(result.body)
-            cy.log("Sold: ", result.body.sold)
-        })
-    })
-  
-  
-  
+  beforeEach(() => {
+    cy.visit('https://automationteststore.com/')
   })
+
+    it('Check API Request', () => {
+      cy.request('https://petstore.swagger.io/v2/store/inventory').then((result)=>{
+
+        expect(result).to.have.property('status', 200)
+        expect(result.body).to.not.be.null
+
+        Object.entries(result.body).forEach(element =>{
+          cy.log(element[0] +' ' + element[1])
+        })
+      })
+    })
+})
